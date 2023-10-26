82c395a088ddf48e09ff7caeb3951a4698fb22b2
Maduabuchi Ginikachukwu Emmanuel <106032738+lightwayz@users.noreply.github.com>
2023-10-26 05:29:00 +0100

    Update view.py

--- app/view.py
+++ app/view.py
@@ -3,7 +3,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
 
 class MainWindow(BoxLayout):
-    username = StringProperty("first.samuel")
+    username = StringProperty("mandos")
     role = StringProperty("user")
     def __init__(self, **kw):
         super().__init__(**kw)
